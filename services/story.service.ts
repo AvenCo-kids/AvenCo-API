2d2c2e6c7e2dd8040fa1b89fadb202db7ebdb6b4
guillaume-lebegue <guillaume.lebegue@epitech.eu>
2020-12-02 16:47:52 +0100

    [ADD] story.service comments [ADD] story.model comments

--- services/story.service.ts
+++ services/story.service.ts
@@ -5,12 +5,24 @@ import { ObjectId } from "https://deno.land/x/mongo@v0.20.0/bson/mod.ts";
 const database = db.getDatabase;
 const stories = database.collection<Story>('stories');
 
-export const createStory = async (newStory: NewStory) => {
+/**
+ * Create a new story and return it's id
+ * @param {NewStory} newStory - Story to create
+ * @returns {ObjectId} - Id of the created story
+ */
+export const createStory = async (newStory: NewStory): Promise<ObjectId> => {
     const fullStory = newStory as Story;
     const insertedId = await stories.insertOne(fullStory);
-    return (insertedId);
+    return (insertedId as ObjectId);
 }
 
+/**
+ * Search for a story with this id
+ * @param {string} storyId - Id of the story to look for
+ * @returns {Story} - Story found
+ * @throws 'Bad id'
+ * @throws 'Not found'
+ */
 export const findById = async (storyId: string) => {
     var id;
     try {
@@ -26,18 +38,37 @@ export const findById = async (storyId: string) => {
     throw new Error('Not found');
 }
 
+/**
+ * Find stories by name. Can have more than one.
+ * @param {string} name - Name to look for
+ * @param {number} skip - Offset of the list to give
+ * @param {number} limit - How much the list can have
+ * @returns {Story[]}
+ */
 export const findByName = async (name: string, skip: number = 0, limit: number = 10) => {
     const storys = await stories.find({name}).skip(skip).limit(limit).toArray();
 
     return (storys);
 }
 
+/**
+ * Get all stories
+ * @param {number} skip - Offset of the list to give
+ * @param {number} limit - Hove much the list can have
+ * @returns {Story[]}
+ */
 export const findAll = async (skip: number = 0, limit: number = 10) => {
     const storys = await stories.find().skip(skip).limit(limit).toArray();
 
     return (storys);
 }
 
+/**
+ * Delete a story by it's id
+ * @param {string} storyId - Id of the story to delete
+ * @throws 'Bad id'
+ * @throws 'Not found'
+ */
 export const deleteById = async (storyId: string) => {
     var id;
     try {
