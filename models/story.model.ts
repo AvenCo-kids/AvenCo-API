3e231430db9a666a6058041084d8cb85427e2dff
guillaume-lebegue <guillaume.lebegue@epitech.eu>
2020-12-03 11:24:07 +0100

    [UP] Story model

--- models/story.model.ts
+++ models/story.model.ts
@@ -1,12 +1,30 @@
 /** Represent a story in the db */
 export interface Story {
-    _id: {
-        $oid: string;
-    };
+    _id: string;
     name: string;
+    startPoint: StoryNode;
 };
 
 /** Represent a story whithout the elements from the db */
 export interface NewStory {
     name: string;
+    startPoint: StoryNode;
+}
+
+export interface StoryNode {
+    hasScript: boolean;
+    script: string;
+    waitTime: number;
+    speach: string;
+    output: [{
+        outputName: string;
+        params: any;
+    }];
+    node: [{
+        inputName: string;
+        params: any;
+        next: StoryNode;
+        tp: boolean;
+        path: string;
+    }]
 }
