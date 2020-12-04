9da313576b05464b95ed64bfc78cd4841339773b
Amaya <guillaume.le-berre@epitech.eu>
2020-12-04 17:21:43 +0100

    [ADD] services for user

--- models/user.model.ts
+++ models/user.model.ts
@@ -1,11 +1,14 @@
+/** Represent an user in the db */
 export interface User {
     _id: string;
     mail: string;
+    name: string;
     password: string;
 }
 
+/** Represent an user without the lements from the db */
 export interface NewUser {
     mail: string;
-    username: string;
+    name: string;
     password: string;
 }
